11f0bbc54597862b1874e4917b07dd1ce27267aa
Braullio Soares <braulliosoares.bs@gmail.com>
2022-09-22 14:41:07 -0300

    adds setup

---
+++ src/test/java/com/techsolutions/gobarber/GobarberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsolutions.gobarber;
+
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+*//*@SpringBootTest
+class GobarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
